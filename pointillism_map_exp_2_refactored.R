787ed5a2036be3e0261dc07adccc02e127bc61bb
Chris Martin <chrisjmartin03@gmail.com>
2022-01-27 14:14:07 +0000

    Refactoring code and annotated version in Affinity

---
+++ pointillism_map_exp_2_refactored.R
@@ -0,0 +1,170 @@
+#' An experiment with the idea of a pointillist choropleth
+#' More details on the idea: Cartography by Kenneth Field (p356 - 357)
+#' Using ONS garden data as an example
+#' Looking at the percentage of households without gardens in London by MSOA
+#' MSOA is a unit of UK administrative geography that approx. corresponds to
+#'  a neighborhood.
+
+library(tidyverse)  # for data wrangling and plotting
+library(sf)         # for processing and plotting geospatial data
+
+# ******************************************************************************
+# Read in the data
+# ******************************************************************************
+
+# read in ONS garden data
+gardens <- readxl::read_xlsx("../datasets/ons_green_space/osprivateoutdoorspacereferencetables.xlsx",
+                             sheet = "msoa_garden_in") %>%
+  janitor::clean_names() %>%
+  select(msoa_code, region_name,
+         perc_garden = percentage_of_addresses_with_private_outdoor_space)
+
+
+# read in MSOA boundaries
+lad_bound <- read_sf("../datasets/msoas/Middle_Layer_Super_Output_Areas_(December_2011)_Boundaries_Generalised_Clipped_(BGC)_EW_V3.shp") %>%
+  janitor::clean_names()
+
+# ******************************************************************************
+# Clean the data
+# ******************************************************************************
+
+# create single table with both ONS garden and spatial data
+gardens_spatial <- lad_bound %>%
+  left_join(gardens, by = c("msoa11cd" = "msoa_code")) %>%
+
+  # focus on London (as per plot in Cartography by Kenneth Field)
+  filter(region_name == "London") %>%
+
+  # bin garden data so a discrete colour scale can be used in the plots
+  mutate(perc_garden_bin = cut(perc_garden,
+                               breaks = seq(0,1,0.1)))
+
+# ******************************************************************************
+# Process the data for the pointillist choropleth
+# ******************************************************************************
+#
+# Three layers are needed:
+#   1. Filled polygons for small areas.
+#   2. Empty polygons for large areas (so they have the same outline as the
+#       the small areas.
+#   3. Filled dots at the centre of large areas.
+#
+# ******************************************************************************
+
+# define the maximum area (m2) of a polygon that can be considered small
+small_threshold <- 1500000  # selected through experimentation and reviewing
+                            # the impact on aesthetics of the plot
+
+# classify polygons as small (or not small)
+gardens_spatial_grouped <- gardens_spatial %>%
+  select(perc_garden, perc_garden_bin, shape_are) %>%
+  mutate(is_small = if_else(shape_are < small_threshold,
+                            TRUE, FALSE)) %>%
+  group_by(is_small)
+
+# create separate dataframes for small and large polygons
+split_df <- group_split(gardens_spatial_grouped)
+
+# small polygons to plotted as per standard choropleth
+filled_polygons <- split_df[[2]]
+
+# Large polygons to host dots
+empty_polygons <- split_df[[1]]
+
+
+# calculate dot radius so the area of each dot is equal the average area of the
+# small, filled polygons
+ave_polygon_size <- mean(filled_polygons$shape_are)
+radius <- sqrt(ave_polygon_size / pi)
+
+centroids <- st_centroid(empty_polygons) %>% # locate dots at centre of polygons
+  select(perc_garden, perc_garden_bin)
+
+# create dots (as circles around central point of polygons)
+dots <- st_buffer(centroids, dist = radius)
+
+
+# ******************************************************************************
+# Set parameters for plotting
+# ******************************************************************************
+
+bound_width <- 0.25
+bound_colour <- "grey70"
+
+# pallette created with Chroma.js
+# https://gka.github.io/palettes/#/10|s|4d062d|25b164|1|1
+pallette_ten_colour <- c('#52002e', '#700447', '#891e5c', '#a03670', '#b74c85',
+                         '#cf629a', '#e778b0', '#fa91c9', '#ffb0e9', '#ffd1ff')
+
+# ******************************************************************************
+# Plot pointillist choropleth
+# ******************************************************************************
+
+ggplot() +
+
+  # plot filled polygons
+  geom_sf(data = filled_polygons, mapping = aes(fill = perc_garden_bin),
+          size = bound_width, colour = bound_colour) +
+
+  # plot empty polygons
+  geom_sf(data = empty_polygons, fill = "white",
+          colour = bound_colour, size = bound_width) +
+
+  # plot points polygons
+  geom_sf(data = dots, mapping = aes(fill = perc_garden_bin), size = 0) +
+
+  # apply colour scheme and label as percentage without garden
+  scale_fill_manual(values = pallette_ten_colour,
+                    labels = c("0-9", "10-19", "20-29", "30-39", "40-49", "50-59",
+                               "60-69", "70-79", "80-89", "90-100"),
+                    name = "Percentage\nof households\nwithout\na garden",
+                    guide = guide_legend(reverse = TRUE)) +
+
+  # simplify presentation of map
+  ggthemes::theme_map() +
+
+  # add solid background (I think it is removed by theme_map())
+  theme(plot.background = element_rect(fill = "white"))
+
+# output image (A4)
+ggsave("pointillist_choropleth.jpg",
+       units = "mm",
+       height = 210, width = 297)
+
+# output for annotation (A4)
+ggsave("pointillist_choropleth.svg",
+       units = "mm",
+       height = 210, width = 297)
+
+# ******************************************************************************
+# Plot standard choropleth (for comparision)
+# ******************************************************************************
+
+ggplot() +
+
+  # plot filled polygons
+  geom_sf(data = gardens_spatial, mapping = aes(fill = perc_garden_bin),
+          size = bound_width, colour = bound_colour) +
+
+  # apply colour scheme and label as percentage without garden
+  scale_fill_manual(values = pallette_ten_colour,
+                    labels = c("0-9", "10-19", "20-29", "30-39", "40-49", "50-59",
+                               "60-69", "70-79", "80-89", "90-100"),
+                    name = "Percentage\nof households\nwithout\na garden",
+                    guide = guide_legend(reverse = TRUE)) +
+
+  # simplify presentation of map
+  ggthemes::theme_map() +
+
+  # add solid background (I think it is removed by theme_map())
+  theme(plot.background = element_rect(fill = "white"))
+
+# output image (A4)
+ggsave("standard_choropleth.jpg",
+       units = "mm",
+       height = 210, width = 297)
+
+# output for annotation
+ggsave("standard_choropleth.svg",
+       units = "mm",
+       height = 210, width = 297)
